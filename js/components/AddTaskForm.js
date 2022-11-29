5b9c8694777cf328fd3025b16b69401efbb11a16
Cezary <102999499+panpusto@users.noreply.github.com>
2022-11-29 14:18:44 +0100

    update create task function

--- js/components/AddTaskForm.js
+++ js/components/AddTaskForm.js
@@ -1,35 +1,27 @@
 import React, {useState} from 'react';
-import {API_URL, API_KEY} from '../api/constants';
-import {getTasks} from "../api/tasks";
+import {createTask} from "../api/tasks";
 
-const AddTaskForm = ({setTasks}) => {
+const AddTaskForm = ({addNewTask}) => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
-    const submitHandler = (e) => {
+    const newTaskHandler = (e) => {
         e.preventDefault();
-        fetch(`${API_URL}/tasks`, {
-            method: 'POST',
-            headers: {
-                Authorization: API_KEY,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                title,
-                description,
-                status: "open"
-            })
-        })
-            .then(response => response.json())
-            .then(data => getTasks(setTasks))
-            .catch(err => console.log(err))
+
+        const task = {
+        title,
+        description,
+        status: 'open'
+        };
+
+        createTask(task, addNewTask);
     }
 
     return (
         <div className="card shadow">
             <div className="card-body">
                 <h1 className="card-title">New task</h1>
-                <form onSubmit={submitHandler}>
+                <form onSubmit={newTaskHandler}>
                     <div className="form-group">
                         <input type="text"
                                className="form-control"
